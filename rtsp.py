b489a84c5ffbf4fec7e881c195c20d8e2756ce7b
Iyad Ahmed <cgonfire@outlook.com>
2021-04-18 02:04:48 +0200

    add function rtsp_request

--- rtsp.py
+++ rtsp.py
@@ -6,42 +6,33 @@ from urllib.parse import urlparse
 url = urlparse("rtsp://admin:admin@192.168.1.6:8554/live")
 address = (url.hostname, url.port)
 
-def rtsp_request(method: str, headers: dict) -> bytes:
-    request = [method + " / " + "RTSP/1.0"]
-    headers = [f"{key}: {repr(headers[key])}" for key in headers]
-    return "\r\n".join(request + headers + ["\r\n"]).encode()
-
-    
-
-lines = ["OPTIONS / RTSP/1.0",
-         "CSeq: 1",
-         #"Require: implicit-play",
-         #"Proxy-Require: gzipped-messages",
-         "\r\n"]
-op_request = "\r\n".join(lines).encode()
-
-r1 = rtsp_request("OPTIONS",
-                   {"CSeq": 1,
-                    "Require": implicit-play,
-                    "Proxy-Require": gzipped-messages
-                    }
-                   )
-
 sock = socket.create_connection(address, timeout=1)
 
-sock.send(op_request)
+def rtsp_request(method: str, headers: dict) -> bytes:
+    request = method + b" / " + b"RTSP/1.0"
+    request += b"\r\n".join(bytes(f"{key}: {repr(headers[key])}") for key in headers)
+    request += b"\r\n"
+    return request
+    # return "\r\n".join(request + headers + ["\r\n"]).encode()
+
+
+r1 = rtsp_request(b"OPTIONS",
+                  {"CSeq": 1,
+                   "Require": "implicit-play",
+                   "Proxy-Require": "gzipped-messages"
+                   }
+                  )
+
+sock.send(r1)
 response = sock.recv(4096)
 if response:
     status = response.decode().split("\r\n")[0].split(' ')[1]
     print(response.decode())
 
 
-lines = ["DESCRIBE / RTSP/1.0",
-         "CSeq: 2",
-         "\r\n"]
+r2 = rtsp_request(b"DESCRIBE", {"CSeq": 2})
 
-r = "\r\n".join(lines).encode()
-sock.send(r)
+sock.send(r2)
 res = sock.recv(4096)
 if res:
     print(res.decode())
